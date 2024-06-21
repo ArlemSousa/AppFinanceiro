9495095f6a1aa83ee5bfde6e1f403cc4fd8c9c3a
Francisco Arlem <113395833+ArlemSousa@users.noreply.github.com>
2024-06-20 22:48:03 -0300

    Criados os requests

---
+++ Fina.Core/Requests/Request.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests
+{
+
+
+    public class CreateCategoryRequest : Request
+    {
+
+    }
+
+    public abstract class Request
+    {
+        public string? UserId { get; set; }
+    }
+}
